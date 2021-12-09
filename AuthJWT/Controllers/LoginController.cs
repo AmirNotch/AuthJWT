3d0ccbeeccf35081d6963bea818290f04c57c5b4
Amir Ibragimov <ibragimov-amir@list.ru>
2021-12-09 16:31:17 +0600

    Controller: LoginController.cs

---
+++ AuthJWT/Controllers/LoginController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Claims;
+using System.Text;
+using AuthJWT.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthJWT.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel userModel)
+        {
+            var user = Authenticate(userModel);
+
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+
+            return NotFound("User not found");
+        }
+
+        private string Generate(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.Email, user.EmailAddress),
+                new Claim(ClaimTypes.GivenName, user.GivenName),
+                new Claim(ClaimTypes.Surname, user.SurName),
+                new Claim(ClaimTypes.Role, user.Role),
+            };
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel Authenticate(UserModel userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault(o =>
+                o.UserName.ToLower() == userLogin.UserName.ToLower() && o.Password == userLogin.Password);
+
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+            return null;
+        }
+    }
+}
