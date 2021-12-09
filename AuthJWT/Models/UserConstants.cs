b8d8534b197d0a9b17891077c06090dc4d60322b
Amir Ibragimov <ibragimov-amir@list.ru>
2021-12-09 16:30:48 +0600

    Startup.cs connected all validations Models: UserConstants.cs UserLogin.cs UserModel.cs

---
+++ AuthJWT/Models/UserConstants.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace AuthJWT.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel()
+            {
+                UserName = "jason_admin", 
+                EmailAddress = "jason.admin@email.com", 
+                Password = "My_password",
+                GivenName = "Jason", 
+                SurName = "Bryant", 
+                Role = "Administrator"
+            },
+            new UserModel()
+            {
+                UserName = "elise_seller", 
+                EmailAddress = "elise.sellar@email.com", 
+                Password = "My_password",
+                GivenName = "Elise", 
+                SurName = "Lambert", 
+                Role = "Seller"
+            }
+        };
+    }
+}
